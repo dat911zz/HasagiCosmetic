0d7eeb78fc3eaf3a9c143fc58b07455d7b1b698f
Huy Van Pham <87406306+huypv2002@users.noreply.github.com>
2023-05-09 15:27:31 +0700

    List Order

---
+++ ZCosmetic/source/app/Views/list-order.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Brancy - Cosmetic & Beauty Salon Website Template</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="Brancy - Cosmetic & Beauty Salon Website Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords"
+        content="bootstrap, ecommerce, ecommerce html, beauty, cosmetic shop, beauty products, cosmetic, beauty shop, cosmetic store, shop, beauty store, spa, cosmetic, cosmetics, beauty salon" />
+    <meta name="author" content="codecarnival" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/images/favicon.webp">
+
+    <!-- CSS (Font, Vendor, Icon, Plugins & Style CSS files) -->
+
+    <!-- Font CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS (Bootstrap & Icon Font) -->
+    <link rel="stylesheet" href="../assets/css/vendor/bootstrap.min.css">
+
+    <!-- Plugins CSS (All Plugins Files) -->
+    <link rel="stylesheet" href="../assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="../assets/css/plugins/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/plugins/fancybox.min.css">
+    <link rel="stylesheet" href="../assets/css/plugins/nice-select.css">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="../assets//css//listorder.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+    <section class="page-header-area pt-10 pb-9" data-bg-color="#FFF3DA">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="page-header-st3-content text-center text-md-start">
+                        <ol class="breadcrumb justify-content-center justify-content-md-start" style="background: none;">
+                            <li class="breadcrumb-item"><a class="text-dark" href="../index.php">Home</a></li>
+                            <li class="breadcrumb-item active text-dark" aria-current="page">Account</li>
+                        </ol>
+                        <h2 class="page-header-title">Account</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-wrap">
+                        <table class="table">
+                            <thead class="thead-primary">
+                                <tr>
+                                    <th>Sản Phẩm</th>
+                                    <th>Tên Sản Phẩm</th>
+                                    <th>Giá</th>
+                                    <th>Số Lượng</th>
+                                    <th>Giá Bán</th>
+                                    <th>Trạng Thái</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="alert" role="alert">
+                                    <td>
+                                        <img class="img" src="../assets/images/shop/1.webp"></img>
+                                    </td>
+                                    <td>
+                                        <div class="email">
+                                            <span>Sneakers Shoes 2020 For Men </span>
+                                            <span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
+                                        </div>
+                                    </td>
+                                    <td>$44.99</td>
+                                    <td class="quantity">
+                                        <div class="input-group">
+                                            <input type="text" name="quantity"
+                                                class="quantity form-control input-number" value="2" min="1" max="100" readonly>
+                                        </div>
+                                    </td>
+                                    <td>$89.98</td>
+                                    <td>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true"><i class="fa fa-close"></i></span>
+                                        </button>
+                                    </td>
+                                </tr>
+
+                                <tr class="alert" role="alert">
+                                    <td>
+                                        <img class="img" src="../assets/images/shop/1.webp"></img>
+                                    </td>
+                                    <td>
+                                        <div class="email">
+                                            <span>Sneakers Shoes 2020 For Men </span>
+                                            <span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
+                                        </div>
+                                    </td>
+                                    <td>$30.99</td>
+                                    <td class="quantity">
+                                        <div class="input-group">
+                                            <input type="text" name="quantity"
+                                                class="quantity form-control input-number" value="1" min="1" max="100" readonly>
+                                        </div>
+                                    </td>
+                                    <td>$30.99</td>
+                                    <td>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true"><i class="fa fa-close"></i></span>
+                                        </button>
+                                    </td>
+                                </tr>
+
+                                <tr class="alert" role="alert">
+                                    <td>
+                                        <img class="img" src="../assets/images/shop/1.webp"></img>
+                                    </td>
+                                    <td>
+                                        <div class="email">
+                                            <span>Sneakers Shoes 2020 For Men </span>
+                                            <span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
+                                        </div>
+                                    </td>
+                                    <td>$35.50</td>
+                                    <td class="quantity">
+                                        <div class="input-group">
+                                            <input type="text" name="quantity"
+                                                class="quantity form-control input-number" value="1" min="1" max="100" readonly>
+                                        </div>
+                                    </td>
+                                    <td>$35.50</td>
+                                    <td>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true"><i class="fa fa-close"></i></span>
+                                        </button>
+                                    </td>
+                                </tr>
+
+                                <tr class="alert" role="alert">
+                                    <td>
+                                        <img class="img" src="../assets/images/shop/1.webp"></img>
+                                    </td>
+                                    <td>
+                                        <div class="email">
+                                            <span>Sneakers Shoes 2020 For Men </span>
+                                            <span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
+                                        </div>
+                                    </td>
+                                    <td>$76.99</td>
+                                    <td class="quantity">
+                                        <div class="input-group">
+                                            <input type="text" name="quantity"
+                                                class="quantity form-control input-number" value="1" min="1" max="100" readonly>
+                                        </div>
+                                    </td>
+                                    <td>$76.99</td>
+                                    <td>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true"><i class="fa fa-close"></i></span>
+                                        </button>
+                                    </td>
+                                </tr>
+
+                                <tr class="alert" role="alert">
+                                    <td class="border-bottom-0">
+                                        <img class="img" src="../assets/images/shop/1.webp"></img>
+                                    </td>
+                                    <td class="border-bottom-0">
+                                        <div class="email">
+                                            <span>Sneakers Shoes 2020 For Men </span>
+                                            <span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
+                                        </div>
+                                    </td>
+                                    <td class="border-bottom-0">$40.00</td>
+                                    <td class="quantity border-bottom-0">
+                                        <div class="input-group">
+                                            <input type="text" name="quantity"
+                                                class="quantity form-control input-number" value="1" min="1" max="100" readonly>
+                                        </div>
+                                    </td>
+                                    <td class="border-bottom-0">$40.00</td>
+                                    <td class="border-bottom-0">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true"><i class="fa fa-close"></i></span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--== End Aside Menu ==-->
+
+    </div>
+    <!--== Wrapper End ==-->
+
+    <!-- JS Vendor, Plugins & Activation Script Files -->
+
+    <!-- Vendors JS -->
+    <script src="../assets/js/vendor/modernizr-3.11.7.min.js"></script>
+    <script src="../assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="../assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="../assets/js/vendor/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="../assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="../assets/js/plugins/fancybox.min.js"></script>
+    <script src="../assets/js/plugins/jquery.nice-select.min.js"></script>
+
+    <!-- Custom Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
